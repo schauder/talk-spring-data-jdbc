f62a065432c3f2c17789f06c1545f50a82af0732
Jens Schauder <jschauder@pivotal.io>
2018-03-22 16:26:33 +0100

    Created a very simple domain and exercise it in a test.

---
+++ src/main/java/de/schauderhaft/spring/data/jdbc/talk/Workout.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.schauderhaft.spring.data.jdbc.talk;
+
+import lombok.Value;
+import org.springframework.data.annotation.Id;
+
+/**
+ * Domain class representing a workout
+ *
+ * @author Jens Schauder
+ */
+// tag::main[]
+public class Workout {
+
+	@Id
+	Long id;
+	String name;
+	Focus focus;
+}
+// end::main[]
