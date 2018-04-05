9fad6e8df0d634bb5507b9153d77d8df3e0f1ec9
Jens Schauder <jschauder@pivotal.io>
2018-04-05 12:08:58 +0200

    Added MyBatis example

--- src/main/java/de/schauderhaft/spring/data/jdbc/talk/SpringleticsConfiguration.java
+++ src/main/java/de/schauderhaft/spring/data/jdbc/talk/SpringleticsConfiguration.java
@@ -15,15 +15,23 @@
  */
 package de.schauderhaft.spring.data.jdbc.talk;
 
-import org.springframework.context.ApplicationListener;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jdbc.mapping.event.BeforeSave;
+import org.springframework.data.jdbc.core.DataAccessStrategy;
+import org.springframework.data.jdbc.mapping.model.JdbcMappingContext;
+import org.springframework.data.jdbc.mybatis.MyBatisContext;
+import org.springframework.data.jdbc.mybatis.MyBatisDataAccessStrategy;
 import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
 
 // tag::main[]
 @Configuration
@@ -32,14 +40,55 @@ import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 public class SpringleticsConfiguration {
 
 	@Bean
-	public NamedParameterJdbcTemplate namedParameterJdbcTemplate() {
-		return new NamedParameterJdbcTemplate(
-				new EmbeddedDatabaseBuilder()
-						.setType(EmbeddedDatabaseType.HSQL)
-						.addScript("schema.sql")
-						.build());
+	public DataSource dataSource() {
+		return new EmbeddedDatabaseBuilder()
+				.setType(EmbeddedDatabaseType.HSQL)
+				.addScript("schema.sql")
+				.build();
+	}
+
+	@Bean
+	public NamedParameterJdbcTemplate namedParameterJdbcTemplate(DataSource db) {
+		return new NamedParameterJdbcTemplate(db);
 	}
 	// end::main[]
 
+	// tag::mybatis1[]
+	@Bean
+	SqlSessionFactoryBean sessionFactory(DataSource db) {
+
+		org.apache.ibatis.session.Configuration config =
+				new org.apache.ibatis.session.Configuration();
+		config.getTypeAliasRegistry()
+				.registerAlias("MyBatisContext", MyBatisContext.class);
+
+		config.getTypeAliasRegistry().registerAlias(Workout.class);
+		config.addMapper(WorkoutMapper.class);
+
+		SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+		bean.setDataSource(db);
+		bean.setConfiguration(config);
+
+		return bean;
+	}
+	// end::mybatis1[]
+
+	// tag::mybatis2[]
+	@Bean
+	SqlSessionTemplate session(SqlSessionFactory factory) {
+		return new SqlSessionTemplate(factory);
+	}
+	// end::mybatis2[]
+
+	// tag::mybatis3[]
+	@Bean
+	DataAccessStrategy dataAccessStrategy(
+			JdbcMappingContext context,
+			SqlSession sqlSession
+	) {
+		return MyBatisDataAccessStrategy
+				.createCombinedAccessStrategy(context, sqlSession);
+	}
+	// tag::mybatis3[]
 }
 
