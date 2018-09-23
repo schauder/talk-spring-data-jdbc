91c94865fcfd9a3495e93d885715f5e740b5384a
Jens Schauder <jschauder@pivotal.io>
2018-09-23 18:37:19 -0400

    fixed schema creation

--- src/main/resources/create-schema.sql
+++ src/main/resources/create-schema.sql
@@ -7,66 +7,6 @@ CREATE TABLE Customer (
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 create TABLE Address (
   customer     INTEGER,
   customer_key VARCHAR(200),
@@ -78,3 +18,8 @@ create TABLE Book (
   id     IDENTITY,
   name         VARCHAR(200)
 );
+
+create TABLE Book_Author (
+  book INTEGER,
+  author INTEGER
+)
